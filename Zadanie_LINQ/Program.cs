34b907c039ec7946dacea48f306a106d9f35cb11
Skowoo <skowoo@tlen.pl>
2023-05-26 15:42:53 +0200

    LINQ method syntax in progress

--- Zadanie_LINQ/Program.cs
+++ Zadanie_LINQ/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            string[] lista = {"foo", "bar", "baz", "sra", "gra"};
+            Console.WriteLine(FilterAndSort(lista, "ba").ToList());
+        }
+
+        public static IEnumerable<string> FilterAndSort(IEnumerable<string> inValues, string pattern)
+        {
+            return inValues
+                .Where(inValues => inValues.Contains(pattern))
+                .OrderBy(inValues => inValues)
+                ;
         }
     }
 }
