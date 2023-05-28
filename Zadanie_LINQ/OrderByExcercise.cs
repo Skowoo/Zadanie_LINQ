e2b0ec5002d392481e42eb054c436035f60c7ff5
Skowoo <skowoo@tlen.pl>
2023-05-28 10:21:41 +0200

    OrderBy exercise done

---
+++ Zadanie_LINQ/OrderByExcercise.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_LINQ
+{
+    public static class OrderByExcercise
+    {
+        public static void PseudoMain()
+        {
+
+        }
+
+        public interface Name
+        {
+            string First { get; }
+            string Middle { get; }
+            string Last { get; }
+        }
+
+        public static class OrderBy1
+        {
+            // Return the provided list of names, ordered by Last, in
+            // descending order.
+            public static IEnumerable<Name> SortNames(IEnumerable<Name> names)
+            {
+                return names.OrderBy(name => name.Last).Reverse();
+            }
+        }
+    }
+}
