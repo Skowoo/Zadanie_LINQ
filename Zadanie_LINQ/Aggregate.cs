6de7927c860c7a386c2eb1f6285cd019a1c83094
Skowoo <skowoo@tlen.pl>
2023-05-28 11:20:50 +0200

    Aggregare excercise done

---
+++ Zadanie_LINQ/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_LINQ
+{
+    public static class Aggregate
+    {
+        public static void PseudoMain()
+        {
+            string[] input = { "abcd", "abcde", "abcdef" };
+            Console.WriteLine(TotalCharactersInSequence(input));
+        }
+
+        public static int TotalCharactersInSequence(IEnumerable<string> words)
+        {
+            return words.Aggregate(0, (charCounter, wordFromArray) => charCounter + wordFromArray.Length);
+        }
+    }
+}
