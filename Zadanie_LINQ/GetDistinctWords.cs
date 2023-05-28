a4abdb4256279085ba670d490c11f87751532df6
Skowoo <skowoo@tlen.pl>
2023-05-28 10:16:42 +0200

    Get Distinct Short Words excercise done

---
+++ Zadanie_LINQ/GetDistinctWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_LINQ
+{
+    public static class GetDistinctWords
+    {
+        public static void PseudoMain()
+        {
+
+        }
+
+        // Return all distinct words that have less than four letters in them.
+        public static IEnumerable<string> GetDistinctShortWords(IEnumerable<string> words)
+        {
+            return words.Where(word => word.Length < 4).Distinct();
+        }
+    }
+}
