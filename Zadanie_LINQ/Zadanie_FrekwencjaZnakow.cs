f70ea94dd0e9b3ea1858fa7053c7d3bc2dac8f6f
Skowoo <skowoo@tlen.pl>
2023-05-29 11:30:47 +0200

    Next steps of assignment

--- Zadanie_LINQ/Zadanie_FrekwencjaZnakow.cs
+++ Zadanie_LINQ/Zadanie_FrekwencjaZnakow.cs
@@ -10,15 +10,15 @@ namespace Zadanie_LINQ
     {
         public static Dictionary<char, int> existingChars = new Dictionary<char, int>();
 
-        public static void Zadanie()
+        public static void Main()
         {
             int queryNumber = Int32.Parse(Console.ReadLine());
             if (queryNumber < 1 | queryNumber > 100) return;
 
-            //BeginningOfSingleQuery:
+            BeginningOfSingleQuery:
             queryNumber--;
 
-            string input = Console.ReadLine().ToLower();
+            char[] input = Console.ReadLine().ToLower().ToCharArray();
             if (input.Length > 1000) return;
 
             string query = Console.ReadLine();
@@ -28,8 +28,40 @@ namespace Zadanie_LINQ
             var inputQuery = input
                 .Select(x => CountChars(x));
 
-            foreach ( var x in existingChars)
-                Console.WriteLine(existingChars.Keys);
+            foreach (var x in inputQuery) { } //Use query to fill the dictionary
+
+            //foreach (var x in existingChars)
+            //    Console.WriteLine($"{x.Key} {x.Value}");
+
+
+
+            //first 3 byfreq asc byletter asc
+
+            var outputQuery = existingChars.Select(x => x);
+
+            if (queryArray.Length == 4)
+            {
+                if (queryArray[2] == "byfreq" && queryArray[3] == "asc")
+                    outputQuery = outputQuery.OrderBy(x => x.Value);
+                else if (queryArray[2] == "byfreq" && queryArray[3] == "desc")
+                    outputQuery = outputQuery.OrderByDescending(x => x.Value);
+                else if (queryArray[2] == "byletter" && queryArray[3] == "asc")
+                    outputQuery = outputQuery.OrderBy(x => x.Key);
+                else if (queryArray[2] == "byletter" && queryArray[3] == "desc")
+                    outputQuery = outputQuery.OrderByDescending(x => x.Key);
+            }
+
+            if (queryArray[0] == "first")
+                outputQuery = outputQuery.Take(int.Parse(queryArray[1]));
+            else
+                outputQuery = outputQuery.TakeLast(int.Parse(queryArray[1]));
+
+
+            foreach (var x in outputQuery)
+                Console.WriteLine($"{x.Key} {x.Value}");
+
+            existingChars.Clear();
+            if (queryNumber > 0) goto BeginningOfSingleQuery;
         }
 
         public static char CountChars(char input)
