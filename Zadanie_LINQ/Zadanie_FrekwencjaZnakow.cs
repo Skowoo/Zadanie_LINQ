9e6af9e6389ee42801d080fbfcf17753fd3f7410
Skowoo <skowoo@tlen.pl>
2023-05-29 10:27:57 +0200

    Working on letters counting

---
+++ Zadanie_LINQ/Zadanie_FrekwencjaZnakow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_LINQ
+{
+    public class Zadanie_FrekwencjaZnakow
+    {
+        public static Dictionary<char, int> existingChars = new Dictionary<char, int>();
+
+        public static void Zadanie()
+        {
+            int queryNumber = Int32.Parse(Console.ReadLine());
+            if (queryNumber < 1 | queryNumber > 100) return;
+
+            //BeginningOfSingleQuery:
+            queryNumber--;
+
+            string input = Console.ReadLine().ToLower();
+            if (input.Length > 1000) return;
+
+            string query = Console.ReadLine();
+
+            string[] queryArray = query.Split(' ');
+
+            var inputQuery = input
+                .Select(x => CountChars(x));
+
+            foreach ( var x in existingChars)
+                Console.WriteLine(existingChars.Keys);
+        }
+
+        public static char CountChars(char input)
+        {
+            if (existingChars.Keys.Contains(input))
+                existingChars[input]++;
+            else if (Char.IsLetter(input))
+                existingChars.Add(input, 1);
+
+            return input;
+        }
+    }
+}
